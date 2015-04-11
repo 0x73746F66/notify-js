a51a5b24ef17a3a48a8cc2be25c8dc2c0dd7581d
Christopher Langton <chrislangton84@gmail.com>
2015-04-11 16:13:58 +1000

    fixed bugs where default options weren't used on some notifications. added a setConfig method to bulk update default options. added ability to completely disable notifications with a setting at any time. added the ability to only create notifications when the browser/tab is not in focus or is minimised.

--- latest/WebNotify.js
+++ latest/WebNotify.js
@@ -1,17 +1,6 @@
 /**
- * Class WebNotify
- * Usage:
-     var noti = new WebNotify();
-     noti.requestPermission();
-     $(document).on('visibilitychange',function(){
-        if (document.hidden) { //web page is no longer active
-          noti.createNotification('We will be ready when you come back back');
-          console.log('not in focus');
-        } else  {
-          console.log('in focus');
-        }
-      });
- *
+ * @class WebNotify
+ * @version 0.2.0
  */
 (function(window, undefined){
   /**
@@ -26,9 +15,12 @@
     this.isSupported = false;
     this.hasPermission = false;
     this.hasUnity = false;
+    this.disableNotifications = !!options.disableNotifications||false;
+    this.disabledOnTabFocussed = !!options.disabledOnTabFocussed||false;
+    this.inFocus = true;
+    var that = this;
 
     if ('undefined' !== typeof external.getUnityObject) {
-      var that = this;
       this.Unity = external.getUnityObject(options.unityVersion||1.0);
       this.Unity.init({
         name: options.appName||"Javascript.WebNotify",
@@ -38,6 +30,25 @@
     }
 
     try {
+      var hidden, visibilityChange;
+      if (typeof document.hidden !== "undefined") { // Opera 12.10 and Firefox 18 and later support
+        hidden = "hidden";
+        visibilityChange = "visibilitychange";
+      } else if (typeof document.mozHidden !== "undefined") {
+        hidden = "mozHidden";
+        visibilityChange = "mozvisibilitychange";
+      } else if (typeof document.msHidden !== "undefined") {
+        hidden = "msHidden";
+        visibilityChange = "msvisibilitychange";
+      } else if (typeof document.webkitHidden !== "undefined") {
+        hidden = "webkitHidden";
+        visibilityChange = "webkitvisibilitychange";
+      }
+      if (typeof document.addEventListener !== 'undefined' && typeof document[hidden] !== 'undefined') {
+        document.addEventListener(visibilityChange, function(){
+          that.inFocus = !document[hidden];
+        }, false);
+      }
       this.isSupported = !!(/* Safari, Chrome */window.Notification || /* Chrome & ff-html5notifications plugin */window.webkitNotifications || /* Firefox Mobile */navigator.mozNotification || /* IE9+ */(window.external && window.external.msIsSiteMode() !== undefined));
     } catch (e) {}
 
@@ -45,12 +56,24 @@
   };
   WebNotify.fn = WebNotify.prototype = {
     /**
+     * setConfig
+     * @returns WebNotify
+     */
+    setConfig: function setConfig(options){
+      if ('undefined' === typeof options) options = {};
+      this.icon = options.icon||false;
+      this.disableNotifications = !!options.disableNotifications||false;
+      this.disabledOnTabFocussed = !!options.disabledOnTabFocussed||false;
+
+      return this;
+    },
+    /**
      * getWindowsBadge
      * @param url string
      * @param polling integer Allowed:  30, 60, 360 (6 hours), 720 (12 hours), and 1440 (1 day)
      * @returns string
      */
-    getWindowsBadgeMetaTag: function (url, polling) { //for badge schema see: https://msdn.microsoft.com/en-us/library/ie/br212849.aspx
+    getWindowsBadgeMetaTag: function getWindowsBadgeMetaTag(url, polling) { //for badge schema see: https://msdn.microsoft.com/en-us/library/ie/br212849.aspx
       url = url || 'https://www.domain.com/windows-polling-icon-state.xml';
       polling = polling || 30;
       return '<meta name="msapplication-badge" content="frequency='+polling+'; polling-uri='+url+'"/>';
@@ -59,7 +82,7 @@
      * refreshWindowsBadge
      * @returns WebNotify
      */
-    refreshWindowsBadge: function(){
+    refreshWindowsBadge: function refreshWindowsBadge(){
       if (window.external && window.external.msSiteModeRefreshBadge)
         window.external.msSiteModeRefreshBadge();
       return this;
@@ -109,23 +132,23 @@
      */
     createNotification: function createNotification(title, options) {
       if ('undefined' === typeof options) options = {};
-      if (this.isSupported && this.checkPermission() && this.hasPermission) {
+      if (this.isSupported && this.checkPermission() && this.hasPermission && !this.disableNotifications && (!this.disabledOnTabFocussed || (this.disabledOnTabFocussed && !this.inFocus))) {
         var notification;
         if (this.hasUnity) {
-          this.Unity.Notification.showNotification(title, options.body||'', options.icon||null);
+          this.Unity.Notification.showNotification(title, options.body||'', options.icon||this.icon||null);
         } else if (window.Notification) {
           notification = new window.Notification(title, {
-            icon: options.icon || "",
-            body: options.body || "",
-            tag: options.tag || "",
-            dir: options.dir || "ltr",
-            lang: options.lang || "en-US"
+            icon: options.icon||this.icon||"",
+            body: options.body||"",
+            tag: options.tag||"",
+            dir: options.dir||"ltr",
+            lang: options.lang||"en-US"
           });
         } else if (window.webkitNotifications) {
-          notification = window.webkitNotifications.createNotification(options.icon||null, title, options.body||'');
+          notification = window.webkitNotifications.createNotification(options.icon||this.icon||null, title, options.body||'');
           notification.show();
         } else if (navigator.mozNotification) {
-          notification = navigator.mozNotification.createNotification(title, options.body||'', options.icon||null);
+          notification = navigator.mozNotification.createNotification(title, options.body||'', options.icon||this.icon||null);
           notification.show();
         }
         if ('function' === typeof options.onshow){
